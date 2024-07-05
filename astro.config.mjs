35a6830180718f8c19ae3aef33f758f37e4afd24
yuto343 <namawasabi1229@gmail.com>
2024-07-05 17:24:52 +0900

    プライバシーポリシーページの作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), svelte()]
+});
